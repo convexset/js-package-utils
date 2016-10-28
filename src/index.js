b2ddb7bf00d23f0b99ea10b350730cffd1bed7dc
Jeremy Chen <convexset@gmail.com>
2016-10-28 15:03:17 +0800

    style changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-module.exports = factoryPackageUtilities(require("underscore"));
+const underscore = require('underscore');
+module.exports = factoryPackageUtilities(underscore);
 
 function factoryPackageUtilities(_) {
-	function isUndefined(o) {
-		return typeof o === "undefined";
-	}
+	// function isUndefined(o) {
+	// 	return typeof o === 'undefined';
+	// }
 
 	function addImmutablePropertyValue(o, name, value, isEnumerable = true, isConfigurable = false) {
 		Object.defineProperty(o, name, {
@@ -51,9 +52,7 @@ function factoryPackageUtilities(_) {
 
 	function isKindaUncloneable(o) {
 		return ['Null', 'Undefined', 'Function', 'GeneratorFunction', 'Boolean', 'String', 'Number', 'RegExp', 'Symbol']
-			.filter(function(t) {
-				return Object.prototype.toString.call(o) === '[object ' + t + ']';
-			})
+			.filter(t => Object.prototype.toString.call(o) === `[object ${t}]`)
 			.length > 0;
 	}
 
@@ -69,10 +68,8 @@ function factoryPackageUtilities(_) {
 			return new Date(o.getTime());
 		}
 
-		var _o = Object.create(Object.getPrototypeOf(o));
-		_.forEach(Object.getOwnPropertyNames(o), function(k) {
-			Object.defineProperty(_o, k, Object.getOwnPropertyDescriptor(o, k));
-		});
+		const _o = Object.create(Object.getPrototypeOf(o));
+		_.forEach(Object.getOwnPropertyNames(o), k => Object.defineProperty(_o, k, Object.getOwnPropertyDescriptor(o, k)));
 		return _o;
 	}
 
@@ -92,10 +89,10 @@ function factoryPackageUtilities(_) {
 			return o.clone();
 		}
 
-		var _o = Object.create(Object.getPrototypeOf(o));
-		_.forEach(Object.getOwnPropertyNames(o), function(k) {
-			var propertyDescriptor = Object.getOwnPropertyDescriptor(o, k);
-			if (typeof propertyDescriptor.value !== "undefined") {
+		const _o = Object.create(Object.getPrototypeOf(o));
+		_.forEach(Object.getOwnPropertyNames(o), k => {
+			const propertyDescriptor = Object.getOwnPropertyDescriptor(o, k);
+			if (typeof propertyDescriptor.value !== 'undefined') {
 				propertyDescriptor.value = deepCopy(propertyDescriptor.value, useCloneMethod);
 			}
 			Object.defineProperty(_o, k, propertyDescriptor);
@@ -104,7 +101,7 @@ function factoryPackageUtilities(_) {
 	}
 
 	function addImmutablePropertyObject(o, name, childObj, isEnumerable = true, isConfigurable = false) {
-		var _obj = shallowCopy(childObj);
+		const _obj = shallowCopy(childObj);
 		Object.defineProperty(o, name, {
 			get: () => shallowCopy(_obj),
 			enumerable: isEnumerable,
@@ -121,7 +118,7 @@ function factoryPackageUtilities(_) {
 	}
 
 	function addImmutablePropertyArray(o, name, arr, isEnumerable = true, isConfigurable = false) {
-		var _arr = arr.map(x => x);
+		const _arr = arr.map(x => x);
 		Object.defineProperty(o, name, {
 			get: () => _arr.map(x => x),
 			enumerable: isEnumerable,
@@ -138,10 +135,10 @@ function factoryPackageUtilities(_) {
 	}
 
 	function getPrototypeElements(o, _payload = {}) {
-		var oProto = Object.getPrototypeOf(o);
+		const oProto = Object.getPrototypeOf(o);
 		if (oProto) {
-			_.forEach(oProto, function(item, name) {
-				if (typeof _payload[name] === "undefined") {
+			_.forEach(oProto, (item, name) => {
+				if (typeof _payload[name] === 'undefined') {
 					_payload[name] = item;
 				}
 			});
@@ -151,8 +148,8 @@ function factoryPackageUtilities(_) {
 	}
 
 	function filterObject(o, oFilter, inPlace = false) {
-		var ret = (inPlace) ? o : {};
-		_.forEach(o, function(v, k) {
+		const ret = inPlace ? o : {};
+		_.forEach(o, (v, k) => {
 			if (inPlace) {
 				if (!oFilter.hasOwnProperty(k)) {
 					delete ret[k];
@@ -175,37 +172,37 @@ function factoryPackageUtilities(_) {
 		}
 
 		// has keys of source object
-		var filteredDuck = filterObject(examinedDuck, sourceDuck);
+		const filteredDuck = filterObject(examinedDuck, sourceDuck);
 		if (!_.isEqual(
-			Object.keys(filteredDuck).sort(),
-			Object.keys(sourceDuck).sort()
-		)) {
+				Object.keys(filteredDuck).sort(),
+				Object.keys(sourceDuck).sort()
+			)) {
 			return false;
 		}
 
 		// reachable prototype member check
-		var exProto = getPrototypeElements(examinedDuck);
-		var srcProto = getPrototypeElements(sourceDuck);
+		const exProto = getPrototypeElements(examinedDuck);
+		const srcProto = getPrototypeElements(sourceDuck);
 		if (!_.isEqual(
-			Object.keys(exProto).sort(),
-			Object.keys(srcProto).sort()
-		)) {
+				Object.keys(exProto).sort(),
+				Object.keys(srcProto).sort()
+			)) {
 			return false;
 		}
 
 		// reachable prototype member equality check
 		if (checkPrototypeChainEquality) {
 			if (!_.isEqual(
-				filterObject(exProto, srcProto),
-				srcProto
-			)) {
+					filterObject(exProto, srcProto),
+					srcProto
+				)) {
 				return false;
 			}
 		}
 
 		// deep type check
 		// note that keys already match
-		for (var k in sourceDuck) {
+		for (const k in sourceDuck) {
 			if (sourceDuck.hasOwnProperty(k)) {
 				if (!hasDuckTypeEquality(examinedDuck[k], sourceDuck[k], checkPrototypeChainEquality)) {
 					return false;
@@ -217,52 +214,53 @@ function factoryPackageUtilities(_) {
 	}
 
 	function updateDefaultOptionsWithInput(defaultOptions, inputOptions, failOnTypeMismatch = true) {
-		var myOptions = {};
+		const myOptions = {};
 
-		_.forEach(defaultOptions, function(opt, k) {
-			var use_input = true;
-			if ((typeof inputOptions[k] !== "undefined") && !hasDuckTypeEquality(inputOptions[k], opt)) {
+		_.forEach(defaultOptions, (opt, k) => {
+			let useInput = true;
+			if ((typeof inputOptions[k] !== 'undefined') && !hasDuckTypeEquality(inputOptions[k], opt)) {
 				if (failOnTypeMismatch) {
-					throw {
-						error: 'option-type-mismatch',
+					const err = new Error('option-type-mismatch');
+					err.details = {
 						option: k,
 						input: inputOptions[k],
 						default: opt
 					};
+					throw err;
 				}
 			}
-			if (typeof inputOptions[k] === "undefined") {
-				use_input = false;
+			if (typeof inputOptions[k] === 'undefined') {
+				useInput = false;
 			}
-			myOptions[k] = use_input ? inputOptions[k] : opt;
+			myOptions[k] = useInput ? inputOptions[k] : opt;
 		});
 		return myOptions;
 	}
 
 	// Have to do things this way so minification does not mess up the function.name property
-	var p_u = function PackageUtilities() {};
-	var o = new p_u();
+	const _pu = function PackageUtilities() {};
+	const o = new _pu();
 	_.forEach({
-		"addImmutablePropertyFunction": addImmutablePropertyFunction,
-		"addImmutablePropertyValue": addImmutablePropertyValue,
-		"addNonEnumerablePropertyValue": addNonEnumerablePropertyValue,
-		"addImmutablePropertyObject": addImmutablePropertyObject,
-		"addImmutablePropertyArray": addImmutablePropertyArray,
-		"addMutablePropertyObject": addMutablePropertyObject,
-		"addMutablePropertyArray": addMutablePropertyArray,
-		"addPropertyGetter": addPropertyGetter,
-		"addPropertyGetterAndSetter": addPropertyGetterAndSetter,
-		"updateDefaultOptionsWithInput": updateDefaultOptionsWithInput,
-		"hasDuckTypeEquality": hasDuckTypeEquality,
-		"getPrototypeElements": getPrototypeElements,
-		"filterObject": filterObject,
-		"shallowCopy": shallowCopy,
-		"deepCopy": deepCopy,
-		"isKindaUncloneable": isKindaUncloneable,
+		'addImmutablePropertyFunction': addImmutablePropertyFunction,
+		'addImmutablePropertyValue': addImmutablePropertyValue,
+		'addNonEnumerablePropertyValue': addNonEnumerablePropertyValue,
+		'addImmutablePropertyObject': addImmutablePropertyObject,
+		'addImmutablePropertyArray': addImmutablePropertyArray,
+		'addMutablePropertyObject': addMutablePropertyObject,
+		'addMutablePropertyArray': addMutablePropertyArray,
+		'addPropertyGetter': addPropertyGetter,
+		'addPropertyGetterAndSetter': addPropertyGetterAndSetter,
+		'updateDefaultOptionsWithInput': updateDefaultOptionsWithInput,
+		'hasDuckTypeEquality': hasDuckTypeEquality,
+		'getPrototypeElements': getPrototypeElements,
+		'filterObject': filterObject,
+		'shallowCopy': shallowCopy,
+		'deepCopy': deepCopy,
+		'isKindaUncloneable': isKindaUncloneable,
 		// undocumented
-		"_filterObject": filterObject,
-		"_getPrototypeElements": getPrototypeElements
-	}, function(fn, name) {
+		'_filterObject': filterObject,
+		'_getPrototypeElements': getPrototypeElements
+	}, (fn, name) => {
 		addImmutablePropertyFunction(o, name, fn);
 	});
 
